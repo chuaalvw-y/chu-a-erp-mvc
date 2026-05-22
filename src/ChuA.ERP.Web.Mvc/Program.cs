759b9aa152434188c2abee2066ae92e183e842d8
chuaalvw-y <115362773+chuaalvw-y@users.noreply.github.com>
2026-05-22 11:29:11 -0700

    Update Program.cs

--- src/ChuA.ERP.Web.Mvc/Program.cs
+++ src/ChuA.ERP.Web.Mvc/Program.cs
@@ -3,6 +3,7 @@
 // Licensed under the Alvin Wilsen Chan Chua Proprietary Use-Only License.
 // See LICENSE.txt in the project root for full license information.
 
+using ChuA.ERP.Dashboard.Mvc.Infrastructure;
 using ChuA.ERP.Web.Mvc.Extensions;
 
 namespace ChuA.ERP.Web.Mvc;
@@ -15,6 +16,15 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
 
         builder.Services.AddChuAErpMvc(builder.Configuration, builder.Environment);
+
+        //Dashboard
+        builder.Services.AddControllersWithViews();
+        builder.Services.AddChuADashboardMvc(builder.Configuration);  // <-- the module
+
+        builder.Services.AddAuthorization();
+
+        // The module's JS sends antiforgery as a header.
+        builder.Services.AddAntiforgery(o => o.HeaderName = "RequestVerificationToken");
 
         var app = builder.Build();
 
@@ -32,9 +42,18 @@ public class Program
         app.UseAuthentication();
         app.UseAuthorization();
 
+        //app.MapControllerRoute(
+        //    name: "default",
+        //    pattern: "{controller=Dashboard}/{action=Index}/{id?}");
+
+
+        // Make sure your area route is mapped: For Dashboard
+        app.MapControllerRoute(
+            name: "areas",
+            pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}");
         app.MapControllerRoute(
             name: "default",
-            pattern: "{controller=Dashboard}/{action=Index}/{id?}");
+            pattern: "{controller=Home}/{action=Index}/{id?}");
 
         app.Run();
     }
